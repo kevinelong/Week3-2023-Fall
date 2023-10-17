d977cf9f488de163a9c6fcdfdce3f969aec36dd1
kevinelong <kevinelong@gmail.com>
2023-10-17 11:12:02 -0700

    comparison

---
+++ comparison.js
@@ -0,0 +1,9 @@
+// Data-Type String, Number, Boolean true/false
+a = 12
+b = 6
+console.log(a == b) // Two == sign is th comparison operator
+console.log(a != b) // NOT Equal -  ! is the Not Operator, "<>" in other language
+console.log(a > b) //Greater Than
+console.log(a >= b) // Greater than or equal to
+console.log(a < b) // Less Than
+console.log(a <= b) //Less than or eqaul to
