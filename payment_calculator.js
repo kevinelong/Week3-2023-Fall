9f89888415a4c958c656a7b1e9482a608028b99d
kevinelong <kevinelong@gmail.com>
2023-10-19 11:45:16 -0700

    expectation

--- payment_calculator.js
+++ payment_calculator.js
@@ -21,4 +21,5 @@ let apr = 3.3; //Annual percentage rate
 const price = 350000; //price of house
 
 const payment = calculate(term, apr, price)
-console.log(payment);
+console.log(payment); //expected 1714.8654830497117
+
